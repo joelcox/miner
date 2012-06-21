6536c1cd82b3c91e851d5b70674ea885e877948a
Joël Cox <joel@joelcox.nl>
2012-06-21 10:39:10 +0200

    Woops, forgot to remove the .pyc which made tests run perfectly locally, but not in a fresh env.

--- tests/test_hunts.py
+++ tests/test_hunts.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath('..'))
 import unittest
 
 import miner.utils
-import miner.decision
+import miner.classification
 
 
 class TestHunts(unittest.TestCase):
@@ -13,10 +13,10 @@ class TestHunts(unittest.TestCase):
     def test_records_in_single_class(self):
         records = [{'home_owner': True, 'employed': True, 'defaulted': False},
                    {'home_owner': True, 'employed': False, 'defaulted': False}]
-        tree = miner.decision.Hunts('defaulted', records)
+        tree = miner.classification.Hunts('defaulted', records)
         self.assertEquals(tree.records_in_single_class(records), True)
 
         records = [{'home_owner': True, 'employed': True, 'defaulted': False},
                    {'home_owner': True, 'employed': False, 'defaulted': True}]
-        tree = miner.decision.Hunts('defaulted', records)
+        tree = miner.classification.Hunts('defaulted', records)
         self.assertEquals(tree.records_in_single_class(records), False)
