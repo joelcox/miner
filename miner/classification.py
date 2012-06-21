70d8e7d4aba25c8160b06d9f5ee5af85f50d6326
Joël Cox <joel@joelcox.nl>
2012-06-21 10:12:21 +0200

    Removed Hunt's algorithm to classification module. Algos are sorted by their resulting operation, not by their shape, etc.

--- miner/classification.py
+++ miner/classification.py
@@ -60,3 +60,33 @@ class KNearestNeighbor(object):
                 most_seen = (label, count)
 
         return most_seen[0]
+
+
+class Hunts(object):
+
+    def __init__(self, class_key, records):
+        self.records = records
+        self.class_key = class_key
+
+    def converge(self, records=None):
+        if records is None:
+            records = self.records
+
+        if (self.records_in_single_class(records)):
+            self.make_node(records)
+        else:
+            partitions = self.compute_partitions(records)
+            for partition in partitions:
+                self.make_node(partition)
+                self.converge(partition['records'])
+
+    def records_in_single_class(self, records):
+        """Checks whether all the class fields are of
+        the same value"""
+        initial_class_value = records[0][self.class_key]
+
+        for record in records:
+            if record[self.class_key] != initial_class_value:
+                return False
+
+        return True
